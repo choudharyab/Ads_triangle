e99d88057f145566e20f6aba3fd7e46c9eb8c828
Anup Choudhary <anup.choudhary@selltm.com>
2020-05-17 14:55:29 +0530

    added routes

--- src/api/routes/route1.1.js
+++ src/api/routes/route1.1.js
@@ -12,13 +12,57 @@ router.get("/docs", swaggerUi.setup(swaggerDocs, { explorer: true }));
 
 /**
  * @swagger
- * /:
- *  post:
- *    description: Use to request all payment
+ * /create:
+ *    post:
+ *      description: create agency with multiple client
+ *    parameters:
+ *      - name: customer
+ *        description: Name of our customer
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: agencyId
+ *        description: Name of our agencyId
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: address1
+ *        description: Name of our address1
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: address2
+ *        description: Name of our address2
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: state
+ *        description: Name of our state
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: city
+ *        description: Name of our state
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ *      - name: phoneNumber
+ *        description: Name of our state
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: string
+ 
  *    responses:
- *      '200':
- *        description: A successful response
+ *      '201':
+ *        description: Successfully created user
  */
-router.post("/", controllers.createAgency);
+router.post("/create", controllers.createAgency);
 
 module.exports = router
